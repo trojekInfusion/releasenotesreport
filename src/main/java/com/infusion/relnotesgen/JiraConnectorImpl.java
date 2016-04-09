bc5b28ba353c0d7eb70f0175aaec337aea0a38ba
Janusz Wisniowski <jwisniowski@infusion.com>
2016-04-09 04:21:39 +0200

    Added logging to the JIRA client; properly closing JIRA client

--- src/main/java/com/infusion/relnotesgen/JiraConnectorImpl.java
+++ src/main/java/com/infusion/relnotesgen/JiraConnectorImpl.java
@@ -1,6 +1,5 @@
 package com.infusion.relnotesgen;
 
-import com.atlassian.httpclient.api.HttpClient;
 import com.atlassian.jira.rest.client.api.AuthenticationHandler;
 import com.atlassian.jira.rest.client.api.JiraRestClient;
 import com.atlassian.jira.rest.client.api.JiraRestClientFactory;
@@ -15,30 +14,53 @@ import com.google.common.base.Predicate;
 import com.google.common.collect.*;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.net.URI;
 import java.text.MessageFormat;
 import java.util.List;
 
 public class JiraConnectorImpl implements JiraConnector {
-    private final JiraRestClient jiraRestClient;
+    private final static Logger logger = LoggerFactory.getLogger(Configuration.LOGGER_NAME);
+
+    private final Configuration configuration;
 
     public JiraConnectorImpl(final Configuration configuration) {
-        JiraRestClientFactory factory = new AsynchronousJiraRestClientFactory();
-        AuthenticationHandler authenticationHandler = new BasicHttpAuthenticationHandler(
-                configuration.getJiraUsername(),
-                configuration.getJiraPassword());
-
-        jiraRestClient = factory.create(
-                URI.create(configuration.getJiraUrl()),
-                authenticationHandler);
+        this.configuration = configuration;
     }
 
     @Override
     public ImmutableMap<String, Issue> getIssuesIncludeParents(final ImmutableSet<String> issueIds) {
+        try(JiraRestClient jiraRestClient = createJiraRestClient()) {
+            return getIssuesIncludeParentsInternal(jiraRestClient, issueIds);
+        } catch (IOException e) {
+            throw new RuntimeException("Exception while contacting JIRA", e);
+        }
+    }
+
+    private JiraRestClient createJiraRestClient() {
+        JiraRestClientFactory factory = new AsynchronousJiraRestClientFactory();
+
+        AuthenticationHandler authenticationHandler = new BasicHttpAuthenticationHandler(
+                configuration.getJiraUsername(),
+                configuration.getJiraPassword());
+
+        return factory.create(
+                URI.create(configuration.getJiraUrl()),
+                authenticationHandler);
+    }
+
+    private ImmutableMap<String, Issue> getIssuesIncludeParentsInternal(
+            final JiraRestClient jiraRestClient,
+            final ImmutableSet<String> issueIds) {
         SearchRestClient searchClient = jiraRestClient.getSearchClient();
 
         String searchQuery = getSearchJQL(issueIds);
+
+        logger.info("Getting issues for keys: {}", issueIds);
+
         SearchResult searchResult = searchClient.searchJql(searchQuery).claim();
         List<Issue> issues = Lists.newArrayList(searchResult.getIssues());
 
@@ -60,6 +82,7 @@ public class JiraConnectorImpl implements JiraConnector {
                         }
                     }
                 })
+                // Eliminate duplicates that have already been fetched
                 .filter(new Predicate<String>() {
                     @Override
                     public boolean apply(String parentKey) {
@@ -69,10 +92,14 @@ public class JiraConnectorImpl implements JiraConnector {
                 .toSet();
 
         if (!parentKeysToFetch.isEmpty()) {
+            logger.info("Fetching subtasks' parents which haven't been fetched already: {}", parentKeysToFetch);
+
             String parentSearchQuery = getSearchJQL(parentKeysToFetch);
             SearchResult parentSearchResult = searchClient.searchJql(parentSearchQuery).claim();
             Iterables.addAll(issues, parentSearchResult.getIssues());
         }
+
+        logger.info("Fetching issues from JIRA completed. {} issues fetched.", issues.size());
 
         return Maps.uniqueIndex(issues, new Function<Issue, String>() {
             @Override
